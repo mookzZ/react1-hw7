76ecee088e8b6caa66a01bd6b65bff085c9d420c
acided <homejet2007@gmail.com>
2023-08-05 14:47:35 +0600

    hw7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [tasks, setTasks] = useState([])
+    const [taskText, setTaskText] = useState("")
 
-export default App;
+    const addTaskFunction = () => {
+        const newTask = {
+            id: Date.now(),
+            text: taskText,
+        };
+
+        setTasks([...tasks, newTask])
+        setTaskText("")
+    }
+
+    const handleAddTask = (event) => {
+        if (taskText.trim() === "") return
+        if (event.key === 'Enter') {
+            addTaskFunction()
+        }
+    };
+
+    const handleDeleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id))
+    };
+
+    const handleEditTask = (id) => {
+        const editedText = prompt("Edit the task:", tasks.find((task) => task.id === id).text)
+        if (editedText !== null) {
+            setTasks(
+                tasks.map((task) => {
+                    if (task.id === id) {
+                        return {
+                            ...task,
+                            text: editedText,
+                        };
+                    }
+                    return task
+                })
+            );
+        }
+    };
+
+    return (
+        <div className="App">
+            <h1>Todo List</h1>
+            <div className="input-container">
+                <input
+                    type="text"
+                    placeholder="Enter a new task..."
+                    value={taskText}
+                    onKeyPress={handleAddTask}
+                    onChange={(e) => setTaskText(e.target.value)}
+                />
+                <button onClick={addTaskFunction}>Add</button>
+            </div>
+            <ul className="task-list">
+                {tasks.map((task) => (
+                    <li key={task.id}>
+                        <div className={'left'}>
+                            <input type="checkbox"/>
+                            <span>{task.text}</span>
+                        </div>
+                        <div>
+                            <button className={'edit'} onClick={() => handleEditTask(task.id)}>Edit</button>
+                            <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default App
